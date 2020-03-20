b4cbaf96e6f0724e6a44657a1a3b0fe11e4e3b5c
Fgp910 <f.gutierrezpena910@gmail.com>
2020-03-20 15:43:01 +0100

    inicial P4

---
+++ P4/src/units/IPhysicalUnit.java
@@ -0,0 +1,9 @@
+package units;
+
+public interface IPhysicalUnit {
+    boolean canTransformTo(IPhysicalUnit u);
+    double transformTo(double d, IPhysicalUnit u) throws QuantityException;
+    Quantity getQuantity();
+    String abbrev();
+    //IMetricSystem getMetricSystem(); // No implementar de momento en este apartado
+}
