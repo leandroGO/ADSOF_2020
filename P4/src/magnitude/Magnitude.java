d8caaf80151954260b27fe7590f1aaefd69b6246
Fgp910 <f.gutierrezpena910@gmail.com>
2020-04-03 16:09:03 +0200

    KK

--- P4/src/magnitude/Magnitude.java
+++ P4/src/magnitude/Magnitude.java
@@ -1,8 +1,41 @@
 package magnitude;
 
+import magnitude.exceptions.QuantityException;
 import units.IPhysicalUnit;
 
-public abstract class Magnitude implements IMagnitude {
+public class Magnitude implements IMagnitude {
     private double value;
     private IPhysicalUnit unit;
+
+    public Magnitude(double value, IPhysicalUnit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public IMagnitude add(IMagnitude m) throws QuantityException {
+        return new Magnitude(m.transformTo(this.getUnit()).getValue() + this.getValue(),
+                this.getUnit());
+    }
+
+    @Override
+    public IMagnitude subs(IMagnitude m) throws QuantityException {
+        return new Magnitude(m.transformTo(this.getUnit()).getValue() - this.getValue(),
+                this.getUnit());
+    }
+
+    @Override
+    public IMagnitude transformTo(IPhysicalUnit c) throws QuantityException {
+        return new Magnitude(getUnit().transformTo(value,c), c);
+    }
+
+    @Override
+    public IPhysicalUnit getUnit() {
+        return unit;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
 }
